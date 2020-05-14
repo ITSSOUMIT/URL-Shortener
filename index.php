5ce4fed165e9c80bb7a890d8a8d1c90400eb3f7d
ITSSOUMIT <cse.190310186@silicon.ac.in>
2020-05-14 23:02:18 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,162 @@
+<?php 
+
+$servername = 'localhost';
+$username = 'root';
+$password = ''; // on localhost by default there is no password
+$dbname = 'shorty';
+$base_url='http://localhost/shorty/'; // it is your application url
+
+
+
+if(isset($_GET['url']) && $_GET['url']!="")
+{ 
+$url=urldecode($_GET['url']);
+if (filter_var($url, FILTER_VALIDATE_URL)) 
+{
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+} 
+$slug=GetShortUrl($url);
+$conn->close();
+?>
+
+<html>
+<head>
+	<title>URL Shortener</title>
+	<link rel="stylesheet" type="text/css" href="css/style1.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+</head>
+<body>
+	<img class="wave" src="img/wave.png">
+	<div class="container">
+		<div class="img">
+			<img src="img/bg.svg">
+		</div>
+		<div class="login-content">
+				<h2 class="title"><a href="<?php echo $base_url.$slug; ?>" target="_blank"><?php echo $base_url.$slug; ?></a></h2>
+        </div>
+    </div>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
+
+<?php
+} 
+else 
+{
+die("$url is not a valid URL");
+}
+
+}
+else
+{
+?>
+<html>
+<head>
+	<title>URL Shortener</title>
+	<link rel="stylesheet" type="text/css" href="css/style1.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+</head>
+<body>
+	<img class="wave" src="img/wave.png">
+	<div class="container">
+		<div class="img">
+			<img src="img/bg.svg">
+		</div>
+		<div class="login-content">
+            <form>
+				<img src="img/avatar.svg">
+				<h2 class="title">Welcome</h2>
+           		<div class="input-div one">
+           		   <div class="i">
+           		   		<i class="fas fa-user"></i>
+           		   </div>
+           		   <div class="div">
+           		   		<h5>Long URL</h5>
+           		   		<input type="url" name="url" class="input" required>
+           		   </div>
+           		</div>
+            	<input type="submit" class="btn" value="Generate">
+            </form>
+        </div>
+    </div>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
+<?php
+}
+
+
+function GetShortUrl($url){
+ global $conn;
+ $query = "SELECT * FROM url_shorten WHERE url = '".$url."' "; 
+ $result = $conn->query($query);
+ if ($result->num_rows > 0) {
+$row = $result->fetch_assoc();
+ return $row['short_code'];
+} else {
+$short_code = generateUniqueID();
+$sql = "INSERT INTO url_shorten (url, short_code, hits)
+VALUES ('".$url."', '".$short_code."', '0')";
+if ($conn->query($sql) === TRUE) {
+return $short_code;
+} else { 
+die("Unknown Error Occured");
+}
+}
+}
+
+
+
+function generateUniqueID(){
+ global $conn; 
+ $token = substr(md5(uniqid(rand(), true)),0,6); // creates a 6 digit unique short id
+ $query = "SELECT * FROM url_shorten WHERE short_code = '".$token."' ";
+ $result = $conn->query($query); 
+ if ($result->num_rows > 0) {
+ generateUniqueID();
+ } else {
+ return $token;
+ }
+}
+
+
+if(isset($_GET['redirect']) && $_GET['redirect']!="")
+{ 
+$slug=urldecode($_GET['redirect']);
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$url= GetRedirectUrl($slug);
+$conn->close();
+header("location:".$url);
+exit;
+}
+
+
+function GetRedirectUrl($slug){
+ global $conn;
+ $query = "SELECT * FROM url_shorten WHERE short_code = '".addslashes($slug)."' "; 
+ $result = $conn->query($query);
+ if ($result->num_rows > 0) {
+$row = $result->fetch_assoc();
+// increase the hit
+$hits=$row['hits']+1;
+$sql = "update url_shorten set hits='".$hits."' where id='".$row['id']."' ";
+$conn->query($sql);
+return $row['url'];
+}
+else 
+ { 
+die("Invalid Link!");
+}
+}
